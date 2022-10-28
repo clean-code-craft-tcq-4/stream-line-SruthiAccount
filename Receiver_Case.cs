e09f4ec7696161582e193ebf0945ca2811df2427
Shiyam Sundar Sugumar (DC-AE/EDC2 ) <Sugumar.Shiyamsundar@in.bosch.com>
2022-10-28 15:11:03 +0530

    Trial_Commit

---
+++ Receiver_Case.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Receiver_TestCase
+{
+    class Receiver_Case
+    {
+        static void Main(string[] args)
+        {
+            var data = Console.ReadLine();
+            Console.WriteLine(data.ToString());
+        }
+    }
+}
