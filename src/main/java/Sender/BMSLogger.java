cc965b8428912a3edee6d1bf6f173ef08c1cc457
SruthiAccount <88025989+SruthiAccount@users.noreply.github.com>
2022-11-10 10:54:47 +0530

    Delete src directory

--- src/main/java/Sender/BMSLogger.java
+++
@@ -1,9 +0,0 @@
-package Sender;
-
-public class BMSLogger  {
-
-	public void printMessage(String content) {
-		System.out.println(content);
-	}
-
-}
--- src/main/java/Sender/BMSSenderService.java
+++
@@ -1,9 +0,0 @@
-package Sender;
-
-import java.util.List;
-
-public interface BMSSenderService {
-		public List<BMSData> generateBatteryParams(int count);
-		public void sendParamsToConsole();
-
-}
